a330a2674691361fc27b8c075d67c11b849dd24a
zhangzuxing <zhangzuxing1@aliyun.com>
2017-02-12 22:35:34 +0800

    add tmp_test.py

---
+++ tmp_test.py
@@ -0,0 +1,52 @@
+#coding=utf-8;
+
+class AboutPage(wx.Dialog):
+    text = u'''
+    <html>
+    <body bgcolor="#ECFFFF">
+    </body>
+    </html>
+    '''
+
+    def __init__(self, parent):
+        wx.Dialog.__init__(self, parent, -1, u'关于软件',
+                          size=(300, 300) )
+
+        html = HtmlWindow(self)
+        html.SetPage(self.text);
+        self.Centre();
+        self.icon = wx.Icon('icon.ico', wx.BITMAP_TYPE_ICO);
+        self.SetIcon(self.icon);
+        button = wx.Button(self, wx.ID_OK, u"已知晓")
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(html, 1, wx.EXPAND|wx.ALL, 5)
+        sizer.Add(button, 0, wx.ALIGN_CENTER|wx.ALL, 5)
+
+        self.SetSizer(sizer)
+        self.Layout()
+
+
+class HelpPage(wx.Dialog):
+    text2 = u'''
+    <html>
+    </html>
+    '''
+
+    def __init__(self, parent):
+        wx.Dialog.__init__(self, parent, -1, u'软件帮助',
+                          size=(550, 495))
+
+        html = HtmlWindow(self)
+        html.SetPage(self.text2);
+        self.Centre();
+        self.icon = wx.Icon('icon.ico', wx.BITMAP_TYPE_ICO);
+        self.SetIcon(self.icon);
+        button = wx.Button(self, wx.ID_OK, u"已知晓")
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(html, 1, wx.EXPAND|wx.ALL, 5)
+        sizer.Add(button, 0, wx.ALIGN_CENTER|wx.ALL, 5)
+
+        self.SetSizer(sizer)
+        self.Layout()
