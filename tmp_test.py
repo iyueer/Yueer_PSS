7d8bb81e6c951aa616afc0915ad132ef4af7376c
iyueer <iyueer3@gmail.com>
2017-02-12 22:54:40 +0800

    Update tmp_test.py

--- tmp_test.py
+++ tmp_test.py
@@ -25,28 +25,3 @@ class AboutPage(wx.Dialog):
 
         self.SetSizer(sizer)
         self.Layout()
-
-
-class HelpPage(wx.Dialog):
-    text2 = u'''
-    <html>
-    </html>
-    '''
-
-    def __init__(self, parent):
-        wx.Dialog.__init__(self, parent, -1, u'软件帮助',
-                          size=(550, 495))
-
-        html = HtmlWindow(self)
-        html.SetPage(self.text2);
-        self.Centre();
-        self.icon = wx.Icon('icon.ico', wx.BITMAP_TYPE_ICO);
-        self.SetIcon(self.icon);
-        button = wx.Button(self, wx.ID_OK, u"已知晓")
-
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(html, 1, wx.EXPAND|wx.ALL, 5)
-        sizer.Add(button, 0, wx.ALIGN_CENTER|wx.ALL, 5)
-
-        self.SetSizer(sizer)
-        self.Layout()
